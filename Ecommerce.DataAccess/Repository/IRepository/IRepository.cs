c2ee572488830c30af95e3b4338ba8c029f35265
saul basilio <saulbf96@outlook.es>
2025-12-02 15:12:31 -0600

    Implement Repository Interface

--- Ecommerce.DataAccess/Repository/IRepository/IRepository.cs
+++ Ecommerce.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 
 namespace Ecommerce.DataAccess.Repository.IRepository
 {
-   internal  interface IRepository<T> where T : class //esto indica que la interface es generica T debe ser una clase 
+   public  interface IRepository<T> where T : class //esto indica que la interface es generica T debe ser una clase 
        //where T : class  no struct  o tipo primitivo 
     {
         //T- Category
 
         IEnumerable<T> GetAll(); //obtener todos los registros de la tabla
+        //Devuelve un solo registro que cumpla la condición enviada mediante una expresión lambda.
         T Get(Expression<Func<T, bool>> filter); //obtener un solo registro 
+        void Add(T entity);//Agrega una nueva entidad T al contexto, para luego ser guardada en la base de datos:
+        //void Update(T entity);//Actualiza un registro existente. Generalmente:
+        void Remove(T entity);  //Elimina un registro especifico
+        void RemoveRange(IEnumerable<T> entity);//Eliminar vasrios registros al mismo tiempo
+
     }
 }
---
+++ Ecommerce.DataAccess/Repository/Repository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.DataAccess.Repository.IRepository;
+using EcommerceWebApp.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Ecommerce.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class //esto es un repositorio generico para reazar CRUD
+        //es generico porque usa <T>,IRepository esto significa que esta oblicada a implementar los metodos definidos de interfaz
+    {
+        //Agregams una iyeccion de dependencia 
+        private readonly ApplicationDbContext _db; //se al,acena a una instancia  con readonly solo puede asignarse al constructor 
+
+        internal DbSet<T> dbSet;//Representa ala tabla correspondiente  ala entidad T 
+        //constructor 
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;// Asignamos el DbContext recibido por inyección de dependencias al campo privado _db
+            this.dbSet = _db.Set<T>();//obtenemos la tabla 
+            //_db.Categories == dbSet;
+        }
+        public void Add(T entity)// agregamos una nueva entidad al conexto 
+        {
+            dbSet.Add(entity);
+            
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet; //creamos una consulta que parte de la tabla T 
+            query = query.Where(filter);// agregamos filtros 
+            return query.FirstOrDefault(); //regresamos los datos 
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
