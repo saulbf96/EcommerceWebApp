373c996125cf0a09a2f9ba0852b33fdeb76ee13d
saulbf96 <saulbf96@outlook.es>
2025-12-01 21:07:41 -0600

    Repository

---
+++ Ecommerce.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository.IRepository
+{
+   internal  interface IRepository<T> where T : class //esto indica que la interface es generica T debe ser una clase 
+       //where T : class  no struct  o tipo primitivo 
+    {
+        //T- Category
+
+        IEnumerable<T> GetAll(); //obtener todos los registros de la tabla
+        T Get(Expression<Func<T, bool>> filter); //obtener un solo registro 
+    }
+}
